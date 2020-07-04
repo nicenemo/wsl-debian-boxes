58f52032b1bed3295c486e68e94346aea8c26fa6
Hans Kruse <hans.kruse@innovalor.nl>
2020-07-05 00:34:48 +0200

    remove spaces from removed sudo

--- upgrade-distro.sh
+++ upgrade-distro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
- apt-get -y update && \
- apt-get -y upgrade && \
- apt-get -y full-upgrade && \
- apt-get -y autoclean && \
- apt-get -y autoremove
+apt-get -y update && \
+apt-get -y upgrade && \
+apt-get -y full-upgrade && \
+apt-get -y autoclean && \
+apt-get -y autoremove
