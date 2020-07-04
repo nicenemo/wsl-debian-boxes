9d467ea1d44d7f6c2fa399432e074518a14eb260
Hans Kruse <kruse@hanskruse.eu>
2020-07-04 23:25:14 +0200

    Only upgrade.

--- upgrade-distro.sh
+++ upgrade-distro.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
  apt-get -y update && \
  apt-get -y upgrade && \
- apt-get -y autoclean && \
- apt-get -y autoremove && \
- cp ./files/buster_sources.list /etc/apt/sources.list &&\
- apt-get -y update && \
- apt-get -y upgrade && \
- apt-get -y autoclean && \
- apt-get -y autoremove && \
- cp ./files/bullseye_sources.list /etc/apt/sources.list &&\
- apt-get -y update && \
- apt-get -y upgrade && \
  apt-get -y full-upgrade && \
  apt-get -y autoclean && \
  apt-get -y autoremove
