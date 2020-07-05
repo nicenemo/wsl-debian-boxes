539d83a38b3ba39c72c230665fa733c155ce7ac5
Hans Kruse <hans.kruse@innovalor.nl>
2020-07-05 15:58:28 +0200

    Add local bin before existing PATH

--- personalize.sh
+++ personalize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 adduser --disabled-password --gecos "" $1
 ln -s "$2" /home/$1/winhome
-echo 'PATH=$PATH:~/.local/bin' >> /home/$1/.profile
+echo 'PATH=~/.local/bin:$PATH:' >> /home/$1/.profile
 echo "$1 ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$1_user"
